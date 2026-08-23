9d83b6eed0c52da6915713eefa2662ca223e4122
Vipansi Kundral <vipansikundral@gmail.com>
2026-08-23 20:06:02 +0530

    Time: 6 ms (97.08%), Space: 47 MB (64.23%) - LeetHub

--- 1927-sum-game/1927-sum-game.java
+++ 1927-sum-game/1927-sum-game.java
@@ -1,22 +1,19 @@
 class Solution {
     public boolean sumGame(String num) {
-        int n = num.length();
-        int sum1 = 0, sum2 = 0;
-        int q1 = 0, q2 = 0;
+        int n=num.length();
+        int sum1=0,sum2=0;
+        int q1=0,q2=0;
 
-        for (int i = 0; i < n / 2; i++) {
+        for(int i=0; i<n/2;i++){
             char ch = num.charAt(i);
-            if (ch == '?') q1++;
-            else sum1 += ch - '0';
+            if(ch=='?') q1++;
+            else sum1+=ch-'0';
         }
-
-        for (int i = n / 2; i < n; i++) {
+        for(int i=n/2;i<n;i++){
             char ch = num.charAt(i);
-            if (ch == '?') q2++;
-            else sum2 += ch - '0';
+            if(ch=='?')q2++;
+            else sum2+=ch-'0';
         }
-
-
-        return (sum1 - sum2) * 2 != (q2 - q1) * 9;
+        return(sum1-sum2)*2 !=(q2-q1)*9;
     }
 }
